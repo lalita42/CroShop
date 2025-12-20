7e613485fb375c7ad0341e8b9da42fe11ffd6e5b
lalita42 <112718959+lalita42@users.noreply.github.com>
2025-12-20 22:43:38 +0530

    Remove Razorpay order placement route
    Removed Razorpay order placement route from orderRoutes.

--- oneCart/backend/routes/orderRoutes.js
+++ oneCart/backend/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import { allOrders, placeOrder, placeOrderRazorpay, updateStatus, userOrder } from '../controller/orderController.js'
+import { allOrders, placeOrder, updateStatus, userOrder } from '../controller/orderController.js'
+// import { placeOrderRazorpay } from '../controller/orderController.js'
 import {isAuth} from "../middleware/isAuth.js"
 import {adminAuth} from "../middleware/adminAuth.js"
 
 export const orderRoutes = express.Router()
 
 orderRoutes.post("/placeorder" ,isAuth, placeOrder)
-orderRoutes.post("/placeorderbyrazorpay" ,isAuth, placeOrderRazorpay)
+// orderRoutes.post("/placeorderbyrazorpay" ,isAuth, placeOrderRazorpay)
 orderRoutes.post("/userorder" ,isAuth, userOrder)
 
 
