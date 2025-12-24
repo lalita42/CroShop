242b90f23e8338d7e33d126f53a918d45fe447f0
lalita42 <112718959+lalita42@users.noreply.github.com>
2025-12-24 19:40:04 +0530

    Refactor context providers in main.jsx

--- oneCart/frontend/src/main.jsx
+++ oneCart/frontend/src/main.jsx
@@ -9,13 +9,25 @@ import  {ShopContext}  from './Context/ShopContext'
 
 
 createRoot(document.getElementById('root')).render(
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { App } from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthContext } from './Context/AuthContext'
+import { UserContext } from './Context/UserContext'
+import  {ShopContext}  from './Context/ShopContext'
+
+
+
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <AuthContext>
+   <AuthContext>
     <UserContext>
       <ShopContext>
-    <App/>
-    </ShopContext>
-    </UserContext>
+       <App/>
+      </ShopContext>
+     </UserContext>
     </AuthContext>
   </BrowserRouter>
+
 )
